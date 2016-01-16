5b2e67d61e04cd45a0ca17c6f213ad356b6c0657
nofar cohen zedek <nofarcohenzedek@nofars-mac.local>
2016-01-16 05:33:49 -0800

    register almost finished

--- DoGo/RegisterViewController.h
+++ DoGo/RegisterViewController.h
@@ -8,13 +8,32 @@
 
 #import <UIKit/UIKit.h>
 #import "Model.h"
+#import "ProfileDogWalkerViewController.h"
+#import "ProfileDogOwnerViewController.h"
+#import "DogWalkerTabsViewController.h"
+#import "DogOwnerTabsViewController.h"
+#import "UIView+Toast.h"
 
 @interface RegisterViewController : UIViewController
 {
     IBOutlet UIScrollView *scroller;
 }
+
+//@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextBox;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextBox;
 @property (weak, nonatomic) IBOutlet UITextField *usernameTexBox;
-@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextBox;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextBox;
+@property (weak, nonatomic) IBOutlet UITextField *addressTextBox;
+@property (weak, nonatomic) IBOutlet UITextField *cityTextBox;
+@property (weak, nonatomic) IBOutlet UIView *userTypeSubView;
+- (IBAction)signUpClick:(id)sender;
+
+@property (nonatomic) BOOL isOwner;
+@property (nonatomic) User *user;
+@property (nonatomic) UIViewController* childVC;
+
 @property (nonatomic) NSString* usernameValue;
 
 
