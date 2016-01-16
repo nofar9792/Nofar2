089c1f9faf57c5e399d453e103f1a6b7b4a5e293
nofar cohen zedek <nofarcohenzedek@nofars-mac.local>
2016-01-16 07:56:47 -0800

    search UI

---
+++ DoGo/DogWalkerCell.h
@@ -0,0 +1,14 @@
+//
+//  DogWalkerCell.h
+//  DoGo
+//
+//  Created by nofar cohen zedek on 1/16/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DogWalkerCell : UITableViewCell
+
+@end
---
+++ DoGo/DogWalkerCell.m
@@ -0,0 +1,13 @@
+//
+//  DogWalkerCell.m
+//  DoGo
+//
+//  Created by nofar cohen zedek on 1/16/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "DogWalkerCell.h"
+
+@implementation DogWalkerCell
+
+@end
