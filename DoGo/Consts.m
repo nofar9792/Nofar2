2b0cd3b02d4e7c9a2fcc4b9545a69b1a650b6b60
nofar cohen zedek <nofarcohenzedek@nofars-mac.local>
2016-01-18 11:01:21 -0800

    ass

--- DoGo/Consts.m
+++ DoGo/Consts.m
@@ -8,37 +8,78 @@
 
 #import "Consts.h"
 
+
+
 @implementation Consts
 
+
+
 NSString * const USER_TABLE = @"Users";
+
 NSString * const USER_ID = @"userId";
+
 NSString * const USERNAME = @"username";
+
 NSString * const FIRST_NAME = @"firstName";
+
 NSString * const LAST_NAME = @"lastName";
+
 NSString * const PHONE_NUMBER = @"phoneNumber";
+
 NSString * const ADDRESS = @"address";
+
 NSString * const CITY = @"city";
+
 NSString * const IS_DOG_WALKER = @"isDogWalker";
+
 NSString * const DOG_WALKERS_TABLE = @"DOG_WALKERS";
+
 NSString * const AGE = @"age";
+
 NSString * const PRICE_FOR_HOUR = @"priceForHour";
+
 NSString * const IS_COMFORTABLE_ON_MORNING = @"isComfortableOnMorning";
+
 NSString * const IS_COMFORTABLE_ON_AFTERNOON = @"isComfortableOnAfternoon";
+
 NSString * const IS_COMFORTABLE_ON_EVENING = @"isComfortableOnEvening";
+
 NSString * const DOGS_TABLE = @"DOGS";
+
 NSString * const NAME = @"name";
+
 NSString * const PIC_REF = @"picRef";
+
 NSString * const TRIPS_TABLE = @"TRIPS";
+
 NSString * const TRIP_ID = @"tripId";
+
 NSString * const DOG_OWNER_ID = @"dogOwnerId";
+
 NSString * const DOG_WALKER_ID = @"dogWalkerId";
+
 NSString * const START_OF_WALKING = @"startOfWalking";
+
 NSString * const END_OF_WALKING = @"endOfWalking";
+
 NSString * const IS_PAID = @"isPaid";
+
 NSString * const REQUESTS_TABLE = @"REQUESTS";
+
 NSString * const REQUEST_STATUS = @"requestStatus";
+
 NSString * const IMAGES_TABLES = @"IMAGES";
+
 NSString * const IMAGE_NAME = @"imageName";
+
 NSString * const IMAGE_FILE = @"file";
+
+NSString * const LAST_UPDATE_TABLE = @"lastUpdate";
+
+NSString * const TABLE_NAME = @"tableName";
+
+NSString * const LAST_UPDATE_DATE = @"lastUpdateDate";
+
+
 
 @end
